cfd6446e9fecdc182a7f996eed5d4475be875931
wesley <robinson.wesleyd@gmail.com>
2019-01-22 22:28:22 -0800

    customize styles and build

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -2,20 +2,13 @@
 const { resolve } = require('path')
 const app = resolve(__dirname, '../src/app.js')
 const routing = resolve(__dirname, '../src/routing.js')
-const path = resolve(__dirname, 'public/js/')
+const path = resolve(__dirname, '../public/js/')
 const filename = '[name].js'
-
-const rules = [
-  {
-    test: /\.svg$/,
-    loader: 'svg-inline-loader'
-  }
-]
-
+const rules = []
 module.exports = {
   entry: {
     app,
-    routing,
+    // routing,
   },
   output: {
     path,
@@ -25,3 +18,4 @@ module.exports = {
     rules,
   }
 }
+console.log(module.exports)
